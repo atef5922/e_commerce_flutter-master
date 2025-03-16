abc9c848e28e0746e6a6638dd8ae8dd735e7463e
atef5922 <sifat15-5922@diu.edu.bd>
2025-03-16 10:36:23 +0600

    sdcxv

---
+++ lib/screens/home/pages/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+import '../../../common/widgets/app_bar.dart';
+import '../widgets/bottom_nav.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _homeScreens = [
+    Container(
+      height: double.infinity,
+      color: Colors.amber,
+    ),
+    Container(
+      height: double.infinity,
+      color: Colors.red,
+    ),
+    Container(
+      height: double.infinity,
+      color: Colors.lightBlue,
+    ),
+    Container(
+      height: double.infinity,
+      color: Colors.green,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: FAppBar(
+        title: "Mega Shop",
+        firstIcon: Iconsax.notification_bing_outline,
+      ),
+      body: _homeScreens[_currentIndex],
+      bottomNavigationBar: FBottomBar(
+        currentIndex: 0,
+      ),
+    );
+  }
+}
